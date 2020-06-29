baa68468eac31363562cf92a8cfc1f3886c9aaf5
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-29 05:44:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,6 @@ def oxford_comma(array)
   elsif array.length == 3
     last = array.pop.prepend(', and ')
     return array.join(', ')<< last
-  else 
-    last = array.pop.prepend(', and ')
-    return array.join(', ')<< last
+  
   end
 end
